376404403c40f6eb5e906bfbafb86639b93e61a7
okang68 <okang0402@gmail.com>
2021-12-14 17:10:08 +0800

    TinNews

---
+++ app/src/main/java/com/laioffer/tinnews/network/RetrofitClient.java
@@ -0,0 +1,41 @@
+package com.laioffer.tinnews.network;
+
+import com.facebook.stetho.okhttp3.StethoInterceptor;
+
+import java.io.IOException;
+
+import okhttp3.Interceptor;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class RetrofitClient {
+    private static final String API_KEY = "82888043a62447b9bc1feee5ba587f12";
+    private static final String BASE_URL = "https://newsapi.org/v2/";
+
+    public static Retrofit newInstance() {
+        OkHttpClient okHttpClient = new OkHttpClient.Builder()
+                .addInterceptor(new HeaderInterceptor())
+                .addNetworkInterceptor(new StethoInterceptor())
+                .build();
+        return new Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .client(okHttpClient)
+                .build();
+    }
+
+    private static class HeaderInterceptor implements Interceptor {
+        @Override
+        public Response intercept(Chain chain) throws IOException {
+            Request original = chain.request();
+            Request request = original
+                    .newBuilder()
+                    .header("X-Api-Key", API_KEY)
+                    .build();
+            return chain.proceed(request);
+        }
+    }
+}
---
+++ app/src/main/java/com/laioffer/tinnews/ui/search/SearchNewsAdapter.java
@@ -0,0 +1,77 @@
+package com.laioffer.tinnews.ui.search;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.laioffer.tinnews.R;
+import com.laioffer.tinnews.databinding.SearchNewsItemBinding;
+import com.laioffer.tinnews.model.Article;
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SearchNewsAdapter extends RecyclerView.Adapter<SearchNewsAdapter.SearchNewsViewHolder> {
+    interface ItemCallback {
+        void onOpenDetails(Article article);
+    }
+
+    private ItemCallback itemCallback;
+
+    public void setItemCallback(ItemCallback itemCallback) {
+        this.itemCallback = itemCallback;
+    }
+
+    // 1. Supporting data:
+    private List<Article> articles = new ArrayList<>();
+
+    public void setArticles(List<Article> newsList) {
+        articles.clear();
+        articles.addAll(newsList);
+        notifyDataSetChanged();
+    }
+
+    // 2. SearchNewsViewHolder:
+    public static class SearchNewsViewHolder extends RecyclerView.ViewHolder {
+        ImageView itemImageView;
+        TextView itemTitleTextView;
+
+        public SearchNewsViewHolder(@NonNull View itemView) {
+            super(itemView);
+            SearchNewsItemBinding binding = SearchNewsItemBinding.bind(itemView);
+            itemImageView = binding.searchItemImage;
+            itemTitleTextView = binding.searchItemTitle;
+        }
+    }
+
+    // 3. Adapter overrides:
+    @NonNull
+    @Override
+    public SearchNewsViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.search_news_item, parent, false);
+        SearchNewsViewHolder viewHolder = new SearchNewsViewHolder(view);
+        Log.d("SearchNewsViewHolder", viewHolder.toString() + "onCreateViewHolder");
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull SearchNewsViewHolder holder, int position) {
+        Log.d("SearchNewsViewHolder", holder.toString() + "onBindViewHolder");
+        Article article = articles.get(position);
+        holder.itemTitleTextView.setText(article.title);
+        Picasso.get().load(article.urlToImage).resize(200, 200).into(holder.itemImageView);
+        holder.itemView.setOnClickListener(v -> itemCallback.onOpenDetails(article));
+    }
+
+    @Override
+    public int getItemCount() {
+        return articles.size();
+    }
+}
